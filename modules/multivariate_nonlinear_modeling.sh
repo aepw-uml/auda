a5c2f62afbd82bd4b0f0a1b5313111ecc10b48a0
James Chen <jameschan312.cn@gmail.com>
2025-12-08 13:20:42 -0500

    refactor: updated docs

--- modules/multivariate_nonlinear_modeling.sh
+++ modules/multivariate_nonlinear_modeling.sh
@@ -3,3 +3,7 @@
 # Not yet determined
 auda pipe run DS-GDP-PW TF-TRIMMING MD-IF ST-BASIC MD-SVR PL-SVR SHOW \
     '--inputs=upper_trimming_percentage=0.2;degree=5'
+
+# Or this
+auda pipe run DS-GDP-URBAN-POP-PW ST-BASIC MD-PR-2D PL-PR-2D SHOW \
+    --inputs=degree=1
