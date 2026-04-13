244b1f2779f414b74957440fca3322827c6c9bd9
James Chen <jameschan312.cn@gmail.com>
2026-04-13 18:31:19 -0400

    feat: move datasets from the old branch

--- python/dataset/__init__.py
+++ python/dataset/__init__.py
@@ -2,10 +2,14 @@ from common.dataset import DatasetFetcher
 
 from .predicted_features import PredictedFeatures
 from .year_ppc import YearPPC
+from .year_pwg import YearPWG
+from .year_trc import YearTRC
 
 dataset_map: dict[str, type[DatasetFetcher]] = {
+    'YearPWG': YearPWG,
+    'YearTRC': YearTRC,
     'YearPPC': YearPPC,
     'PredictedFeatures': PredictedFeatures,
 }
 
-__all__ = ['PredictedFeatures', 'YearPPC', 'dataset_map']
+__all__ = ['YearPWG', 'YearTRC', 'YearPPC', 'PredictedFeatures', 'dataset_map']
